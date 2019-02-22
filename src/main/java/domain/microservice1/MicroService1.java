35336fb2178cc5d26c44bf47ec96a76216f91a75
Joaquín <jocon@younapp.com>
2019-02-23 00:23:03 +0100

    initial commit

---
+++ src/main/java/domain/microservice1/MicroService1.java
@@ -0,0 +1,12 @@
+package domain.microservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroService1 {
+
+    public static void main(String[] args){
+        SpringApplication.run(MicroService1.class, args);
+    }
+}
