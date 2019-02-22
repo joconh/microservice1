6143258d3de0ed2e3554c4e956ebb3d24e8f516f
Joaquín <jocon@younapp.com>
2019-02-23 00:23:32 +0100

    add swagger config

---
+++ src/main/java/domain/microservice1/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package domain.microservice1.config;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket sadrApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("domain")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .genericModelSubstitutes(ResponseEntity.class)
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Microservice 1")
+                .description("demo")
+                .version("0.0.1-SNAPSHOT")
+                .license("MIT")
+                .licenseUrl("https://opensource.org/licenses/MIT")
+                .build();
+    }
+}
