72f92ad797c3089f25ef89f832c3d9b1e9c6beb3
Joaquín <jocon@younapp.com>
2019-02-24 11:54:03 +0100

    @EnableEurekaClient add

--- src/main/java/domain/microservice1/MicroService1.java
+++ src/main/java/domain/microservice1/MicroService1.java
@@ -2,8 +2,11 @@ package domain.microservice1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroService1 {
 
     public static void main(String[] args){
